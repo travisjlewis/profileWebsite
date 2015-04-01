9c0af8404310a567a49059fb583de4fb9e9f4910
travisjlewis <trav.lewis92@gmail.com>
2015-04-01 15:40:29 -0400

    Adding center box functionality

--- portfolio.js
+++ portfolio.js
@@ -5,12 +5,18 @@ function expand(toExpand)
 	if(!animationHappening) {
 		animationHappening = true;
 		toExpand.classList.add("expandToFull");
-		toExpand.style.zIndex = 0;
+		toExpand.style.zIndex = 1;
 		
 		var expandHandler = function(){
 			animationHappening = false;
 			toExpand.style.width = "100vw";
 			toExpand.style.height = "100vh";
+			if(toExpand.classList.contains("center"))
+			{
+				toExpand.style.top = "0%";
+				toExpand.style.left = "0%";
+			}
+			
 			toExpand.classList.remove("expandToFull");
 			toExpand.onclick = function() {retract(toExpand)};
 			toExpand.removeEventListener("animationend", expandHandler);
@@ -24,15 +30,32 @@ function retract(toRetract)
 {
 	if(!animationHappening) {
 		animationHappening = true;
-		toRetract.classList.add("retractToQuarter");
+		if(toRetract.classList.contains("center")) {
+			toRetract.classList.add("retractToCenter");
+		}
+		else {
+			toRetract.classList.add("retractToQuarter");
+		}
 		
 		var retractHandler = function(){
 			animationHappening = false;
-			toRetract.style.width = "50vw";
-			toRetract.style.height = "50vh";
-			toRetract.classList.remove("retractToQuarter");
+			if(toRetract.classList.contains("center"))
+			{
+				toRetract.style.width = "30vw";
+				toRetract.style.height = "30vh";
+				toRetract.style.top = "35vh";
+				toRetract.style.left = "35vw";
+				toRetract.classList.remove("retractToCenter");
+				toRetract.style.zIndex = 0;
+			}
+			else
+			{
+				toRetract.style.width = "50vw";
+				toRetract.style.height = "50vh";
+				toRetract.classList.remove("retractToQuarter");
+				toRetract.style.zIndex = -1;
+			}
 			toRetract.onclick = function() {expand(toRetract)};
-			toRetract.style.zIndex = -1;
 			toRetract.removeEventListener("animationend", retractHandler);
 		}
 		
